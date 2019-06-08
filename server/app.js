aed7d0a74e517c6a8ca695e8eb943b605cd1256c
Palash Mondal <palashm@live.in>
2019-06-08 21:11:34 +0530

    Setting up GraphQL

--- server/app.js
+++ server/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 // create our Express app
 const app = express();
 app.set('port', process.env.PORT || 4000);
+
+// bind express with graphql
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    // pass in a schema property
+  })
+);
 
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
