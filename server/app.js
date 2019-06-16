dbab26b2d7512b1b2d3d583c5a9deaf2faa54b0a
Palash Mondal <palashm@live.in>
2019-06-16 12:41:45 +0530

    Connecting to mLab

--- server/app.js
+++ server/app.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema');
 
 // create our Express app
 const app = express();
+
+// import environmental variables from our .env file
+require('dotenv').config({ path: 'variables.env' });
+
+// Connect to our Database and handle an bad connections
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+mongoose.connection.once('open', () => {
+  console.log('\nConneted to database');
+});
+mongoose.connection.on('error', err => {
+  console.error(`\n🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+});
+
 app.set('port', process.env.PORT || 4000);
 
 // bind express with graphql
@@ -17,6 +32,6 @@ app.use(
 
 const server = app.listen(app.get('port'), () => {
   const { port } = server.address();
-  console.log(`Express running → PORT ${port}`);
-  console.log(`GraphQL API server at http://localhost:${port}/graphql`);
+  console.log(`\nExpress running → PORT ${port}`);
+  console.log(`\nGraphQL API server at http://localhost:${port}/graphql`);
 });
