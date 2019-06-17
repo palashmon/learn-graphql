cff45b70f2eefdab36e0b4935d7ec1983b6f1974
Palash Mondal <palashm@live.in>
2019-06-17 18:50:28 +0530

    Mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -78,6 +78,28 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+// Add mutations to the GraphQL API
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      resolve(parent, args) {
+        const author = new Author({
+          name: args.name,
+          age: args.age,
+        });
+        return author.save();
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: Mutation,
 });
