c2b7d37465bcde5147a252e80f2e4b1a85a9289b
Palash Mondal <palashm@live.in>
2019-06-20 21:36:29 +0530

    Updating Component State

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,8 +1,31 @@
+/* eslint-disable react/jsx-no-bind */
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { getAuthorsQuery } from '../queries/queries';
 
 class AddBook extends Component {
+  state = {
+    name: '',
+    genre: '',
+    authorId: '',
+  };
+
+  // Submit the book data to db
+  submitForm(e) {
+    e.preventDefault();
+    // console.log(this.state);
+  }
+
+  onBookNameChange(e) {
+    this.setState({ name: e.target.value });
+  }
+  onBookGenreChange(e) {
+    this.setState({ genre: e.target.value });
+  }
+  onBookAuthorChange(e) {
+    this.setState({ authorId: e.target.value });
+  }
+
   displayAuthors() {
     let { loading, authors } = this.props.data;
     if (loading) {
@@ -20,19 +43,19 @@ class AddBook extends Component {
   }
   render() {
     return (
-      <form id="add-book">
+      <form id="add-book" onSubmit={this.submitForm.bind(this)}>
         <div className="field">
           <label>Book name:</label>
-          <input type="text" />
+          <input type="text" onChange={this.onBookNameChange.bind(this)} />
         </div>
         <div className="field">
           <label>Genre:</label>
-          <input type="text" />
+          <input type="text" onChange={this.onBookGenreChange.bind(this)} />
         </div>
         <div className="field">
           <label>Author:</label>
-          <select>
-            <option>Select author</option>
+          <select onChange={this.onBookAuthorChange.bind(this)}>
+            <option>Select Author</option>
             {this.displayAuthors()}
           </select>
         </div>
